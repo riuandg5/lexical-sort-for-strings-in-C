842b0bbd32be68484561d7079bfce9bf7805e994
Rishu Anand <riuandg5@gmail.com>
2017-12-03 16:02:39 +0530

    Update lexical_sort.C

--- lexical_sort.C
+++ lexical_sort.C
@@ -4,25 +4,24 @@
 
 int main()
 {
-    int num,i,j,k,lshort;
-
     //Get number of strings to arrange.
     printf("Enter number of words you want to arrange : ");
+    int num;
     scanf("%d",&num);
 
     //Declare num number of strings with available length of 100 characters.
     char word[num][100];
 
     //Input strings one by one.
-    for(j=0;j<num;j++)
+    for(int j=0;j<num;j++)
     {
-        printf("Enter string %d : ",j+1);
+        printf("Enter word %d : ",j+1);
         scanf("\n%[^\n]",word[j]); //exclude white spaces
     }
 
-    //Method 1 : using strcmp
-    for(k=0;k<num-1;k++)
-        for(j=0;j<num-1;j++)
+    //Bubble sort algorithm
+    for(int i=0,k=0;i<num-1-k;i++,k++)
+        for(int j=0;j<num-1;j++)
         {
             //Compare two strings using strcmp.
             //If 1st string is greater than 2nd then swap strings.
@@ -35,43 +34,9 @@ int main()
             }
         }
 
-    /*
-    //Method 2 : Bubble sort algorithm
-    for(k=0;k<num-1;k++)
-    for(j=0;j<num-1-k;j++)
-    {
-        //Take two strings at a time.
-        //Compare their length.
-
-        //tshort = lenght of shorter string
-        if(strlen(word[j])<=strlen(word[j+1]))
-            lshort=strlen(word[j]);
-        else
-            lshort=strlen(word[j+1]);
-
-        //Compare letter by letter of both strings till shorter string's length.
-        for(i=0;i<lshort;i++)
-        {
-            //If ascii value of ith letter of 1st string is smaller than 2nd then do nothing.
-            if(word[j][i]<word[j+1][i])
-                break;
-
-            //If ascii value of ith letter of 1st string is greater than 2nd then swap strings.
-            else if(word[j][i]>word[j+1][i])
-            {
-                char temp[100]; //Temporary string
-                strcpy(temp,word[j]);
-                strcpy(word[j],word[j+1]);
-                strcpy(word[j+1],temp);
-                break;
-            }
-        }
-    }
-    */
-
     //Print arranged strings one by one.
     printf("\nArranging in dictionary order :\n\n");
-    for(j=0;j<num;j++)
+    for(int j=0;j<num;j++)
         printf("%s\n",word[j]);
 
     return 0;
